437df7610f3990d394f0f27e4bacb7b073fa2f6c
Mariotaku Lee <mariotaku.lee@gmail.com>
2013-12-05 16:14:22 +0800

    improved daydream

--- src/org/mariotaku/twidere/service/NyanDaydreamService.java
+++ src/org/mariotaku/twidere/service/NyanDaydreamService.java
@@ -4,33 +4,27 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Resources;
 import android.service.dreams.DreamService;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 
 import org.mariotaku.twidere.Constants;
 import org.mariotaku.twidere.R;
-import org.mariotaku.twidere.util.NyanSurfaceHelper;
+import org.mariotaku.twidere.view.NyanDaydreamView;
 
 public class NyanDaydreamService extends DreamService implements Constants, OnSharedPreferenceChangeListener {
 
-	private SurfaceView mSurfaceView;
+	private NyanDaydreamView mNyanDaydreamView;
 	private SharedPreferences mPreferences;
-	private NyanSurfaceHelper mHelper;
 
 	@Override
 	public void onAttachedToWindow() {
 		super.onAttachedToWindow();
-		setContentView(R.layout.surface_view);
-		final SurfaceHolder holder = mSurfaceView.getHolder();
-		mHelper = new NyanSurfaceHelper(this);
-		holder.addCallback(mHelper);
-		updateSurface();
+		setContentView(R.layout.nyan_daydream);
+		updateView();
 	}
 
 	@Override
 	public void onContentChanged() {
 		super.onContentChanged();
-		mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+		mNyanDaydreamView = (NyanDaydreamView) findViewById(R.id.nyan);
 	}
 
 	@Override
@@ -38,20 +32,22 @@ public class NyanDaydreamService extends DreamService implements Constants, OnSh
 		super.onCreate();
 		mPreferences = getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE);
 		mPreferences.registerOnSharedPreferenceChangeListener(this);
+		setInteractive(false);
+		setFullscreen(true);
 	}
 
 	@Override
 	public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
 		if (PREFERENCE_KEY_LIVE_WALLPAPER_SCALE.equals(key)) {
-			updateSurface();
+			updateView();
 		}
 	}
 
-	private void updateSurface() {
+	private void updateView() {
 		if (mPreferences == null) return;
 		final Resources res = getResources();
 		final int def = res.getInteger(R.integer.default_live_wallpaper_scale);
-		mHelper.setScale(mPreferences.getInt(PREFERENCE_KEY_LIVE_WALLPAPER_SCALE, def));
+		mNyanDaydreamView.setScale(mPreferences.getInt(PREFERENCE_KEY_LIVE_WALLPAPER_SCALE, def));
 	}
 
 }
