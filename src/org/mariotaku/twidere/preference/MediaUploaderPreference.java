b07492446137a26f20d9027d674b8b447effbf8e
Mariotaku Lee <mariotaku.lee@gmail.com>
2014-02-17 22:01:47 +0800

    renamed two classes

--- src/org/mariotaku/twidere/preference/MediaUploaderPreference.java
+++ src/org/mariotaku/twidere/preference/MediaUploaderPreference.java
@@ -19,111 +19,35 @@
 
 package org.mariotaku.twidere.preference;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.preference.DialogPreference;
 import android.util.AttributeSet;
 
 import org.mariotaku.twidere.Constants;
 import org.mariotaku.twidere.R;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MediaUploaderPreference extends ServicePickerPreference implements Constants {
 
-public class ImageUploaderPreference extends DialogPreference implements Constants, OnClickListener {
-
-	private SharedPreferences mPreferences;
-	private final PackageManager mPackageManager;
-	private ImageUploaderSpec[] mAvailableImageUploaders;
-
-	public ImageUploaderPreference(final Context context) {
-		this(context, null);
+	public MediaUploaderPreference(final Context context) {
+		super(context);
 	}
 
-	public ImageUploaderPreference(final Context context, final AttributeSet attrs) {
-		this(context, attrs, android.R.attr.preferenceStyle);
-	}
-
-	public ImageUploaderPreference(final Context context, final AttributeSet attrs, final int defStyle) {
-		super(context, attrs, defStyle);
-		mPackageManager = context.getPackageManager();
+	public MediaUploaderPreference(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
 	}
 
 	@Override
-	public void onClick(final DialogInterface dialog, final int which) {
-		final SharedPreferences.Editor editor = getEditor();
-		if (editor == null) return;
-		final ImageUploaderSpec spec = mAvailableImageUploaders[which];
-		if (spec != null) {
-			editor.putString(KEY_IMAGE_UPLOADER, spec.cls);
-			editor.commit();
-		}
-		dialog.dismiss();
+	protected String getIntentAction() {
+		return INTENT_ACTION_EXTENSION_UPLOAD_MEDIA;
 	}
 
 	@Override
-	public void onPrepareDialogBuilder(final AlertDialog.Builder builder) {
-		mPreferences = getSharedPreferences();
-		super.onPrepareDialogBuilder(builder);
-		if (mPreferences == null) return;
-		final String component = mPreferences.getString(KEY_IMAGE_UPLOADER, null);
-		final ArrayList<ImageUploaderSpec> specs = new ArrayList<ImageUploaderSpec>();
-		specs.add(new ImageUploaderSpec(getContext().getString(R.string.image_uploader_default), null));
-		final Intent query_intent = new Intent(INTENT_ACTION_EXTENSION_UPLOAD_MEDIA);
-		final List<ResolveInfo> result = mPackageManager.queryIntentServices(query_intent, 0);
-		for (final ResolveInfo info : result) {
-			specs.add(new ImageUploaderSpec(info.loadLabel(mPackageManager).toString(), info.serviceInfo.packageName
-					+ "/" + info.serviceInfo.name));
-		}
-		mAvailableImageUploaders = specs.toArray(new ImageUploaderSpec[specs.size()]);
-		builder.setSingleChoiceItems(mAvailableImageUploaders, getIndex(component), ImageUploaderPreference.this);
-		builder.setNegativeButton(android.R.string.cancel, null);
+	protected String getNoneEntry() {
+		return getContext().getString(R.string.image_uploader_default);
 	}
 
-	private int getIndex(final String cls) {
-		if (mAvailableImageUploaders == null) return -1;
-		if (cls == null) return 0;
-		final int count = mAvailableImageUploaders.length;
-		for (int i = 0; i < count; i++) {
-			final ImageUploaderSpec spec = mAvailableImageUploaders[i];
-			if (cls.equals(spec.cls)) return i;
-		}
-		return -1;
-	}
-
-	static class ImageUploaderSpec implements CharSequence {
-		private final String name, cls;
-
-		ImageUploaderSpec(final String name, final String cls) {
-			this.name = name;
-			this.cls = cls;
-		}
-
-		@Override
-		public char charAt(final int index) {
-			return name.charAt(index);
-		}
-
-		@Override
-		public int length() {
-			return name.length();
-		}
-
-		@Override
-		public CharSequence subSequence(final int start, final int end) {
-			return name.subSequence(start, end);
-		}
-
-		@Override
-		public String toString() {
-			return name;
-		}
+	@Override
+	protected String getNoneValue() {
+		return VALUE_NONE;
 	}
 
 }
--- src/org/mariotaku/twidere/preference/StatusShortenerPreference.java
+++ src/org/mariotaku/twidere/preference/StatusShortenerPreference.java
@@ -19,113 +19,35 @@
 
 package org.mariotaku.twidere.preference;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.preference.DialogPreference;
 import android.util.AttributeSet;
 
 import org.mariotaku.twidere.Constants;
 import org.mariotaku.twidere.R;
 
-import java.util.ArrayList;
-import java.util.List;
+public class StatusShortenerPreference extends ServicePickerPreference implements Constants {
 
-public class TweetShortenerPreference extends DialogPreference implements Constants, OnClickListener {
-
-	private SharedPreferences mPreferences;
-
-	private final PackageManager mPackageManager;
-
-	private TweetShortenerSpec[] mAvailableTweetShorteners;
-
-	public TweetShortenerPreference(final Context context) {
+	public StatusShortenerPreference(final Context context) {
 		this(context, null);
 	}
 
-	public TweetShortenerPreference(final Context context, final AttributeSet attrs) {
-		this(context, attrs, android.R.attr.preferenceStyle);
-	}
-
-	public TweetShortenerPreference(final Context context, final AttributeSet attrs, final int defStyle) {
-		super(context, attrs, defStyle);
-		mPackageManager = context.getPackageManager();
+	public StatusShortenerPreference(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
 	}
 
 	@Override
-	public void onClick(final DialogInterface dialog, final int which) {
-		final SharedPreferences.Editor editor = getEditor();
-		if (editor == null) return;
-		final TweetShortenerSpec spec = mAvailableTweetShorteners[which];
-		if (spec != null) {
-			editor.putString(KEY_TWEET_SHORTENER, spec.cls);
-			editor.commit();
-		}
-		dialog.dismiss();
+	protected String getIntentAction() {
+		return INTENT_ACTION_EXTENSION_SHORTEN_STATUS;
 	}
 
 	@Override
-	public void onPrepareDialogBuilder(final AlertDialog.Builder builder) {
-		super.onPrepareDialogBuilder(builder);
-		mPreferences = getSharedPreferences();
-		if (mPreferences == null) return;
-		final String component = mPreferences.getString(KEY_TWEET_SHORTENER, null);
-		final ArrayList<TweetShortenerSpec> specs = new ArrayList<TweetShortenerSpec>();
-		specs.add(new TweetShortenerSpec(getContext().getString(R.string.tweet_shortener_default), null));
-		final Intent query_intent = new Intent(INTENT_ACTION_EXTENSION_SHORTEN_STATUS);
-		final List<ResolveInfo> result = mPackageManager.queryIntentServices(query_intent, 0);
-		for (final ResolveInfo info : result) {
-			specs.add(new TweetShortenerSpec(info.loadLabel(mPackageManager).toString(), info.serviceInfo.packageName
-					+ "/" + info.serviceInfo.name));
-		}
-		mAvailableTweetShorteners = specs.toArray(new TweetShortenerSpec[specs.size()]);
-		builder.setSingleChoiceItems(mAvailableTweetShorteners, getIndex(component), TweetShortenerPreference.this);
-		builder.setNegativeButton(android.R.string.cancel, null);
+	protected String getNoneEntry() {
+		return getContext().getString(R.string.status_shortener_default);
 	}
 
-	private int getIndex(final String cls) {
-		if (mAvailableTweetShorteners == null) return -1;
-		if (cls == null) return 0;
-		final int count = mAvailableTweetShorteners.length;
-		for (int i = 0; i < count; i++) {
-			final TweetShortenerSpec spec = mAvailableTweetShorteners[i];
-			if (cls.equals(spec.cls)) return i;
-		}
-		return -1;
-	}
-
-	static class TweetShortenerSpec implements CharSequence {
-		private final String name, cls;
-
-		TweetShortenerSpec(final String name, final String cls) {
-			this.name = name;
-			this.cls = cls;
-		}
-
-		@Override
-		public char charAt(final int index) {
-			return name.charAt(index);
-		}
-
-		@Override
-		public int length() {
-			return name.length();
-		}
-
-		@Override
-		public CharSequence subSequence(final int start, final int end) {
-			return name.subSequence(start, end);
-		}
-
-		@Override
-		public String toString() {
-			return name;
-		}
+	@Override
+	protected String getNoneValue() {
+		return VALUE_NONE;
 	}
 
 }
